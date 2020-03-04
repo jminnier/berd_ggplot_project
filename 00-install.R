94eeab35fbe4781bf73236b171ab80d8c2f38316
Jessica Minnier <jessica.minnier@gmail.com>
2020-03-03 17:08:03 -0800

    upload the files

---
+++ 00-install.R
@@ -0,0 +1,23 @@
+
+# install packages needed for workshop
+
+install.packages(c("tidyverse","ggplot2","lubridate",
+                   "janitor","ggridges","gapminder",
+                   "GGally","ggExtra","ggthemes","hrbrthemes",
+                   "ggrepel","corrplot","ggpubr",
+                   "pheatmap","plotly","here"))
+
+library(tidyverse)
+library(lubridate)
+library(janitor)
+library(ggridges)
+library(gapminder)
+library(GGally)
+library(ggExtra)
+library(ggthemes)
+library(hrbrthemes)
+library(ggrepel)
+library(corrplot)
+library(ggpubr)
+library(pheatmap)
+library(plotly)
