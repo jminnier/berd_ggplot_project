95504db0b3604221f1c2f24a29065f54ecefb71a
Jessica Minnier <jessica.minnier@gmail.com>
2020-03-04 10:50:34 -0800

    remove hrbrthemes in case it doesn't work

--- 00-install.R
+++ 00-install.R
@@ -3,7 +3,7 @@
 
 install.packages(c("tidyverse","ggplot2","lubridate",
                    "janitor","ggridges","gapminder",
-                   "GGally","ggExtra","ggthemes","hrbrthemes",
+                   "GGally","ggExtra","ggthemes",
                    "ggrepel","corrplot","ggpubr",
                    "pheatmap","plotly","here"))
 
@@ -15,9 +15,14 @@ library(gapminder)
 library(GGally)
 library(ggExtra)
 library(ggthemes)
-library(hrbrthemes)
 library(ggrepel)
 library(corrplot)
 library(ggpubr)
 library(pheatmap)
 library(plotly)
+
+# You may have issues installing this package, 
+# if it doesn't work you just won't be able to use the themes from this package
+# If you're on a newer mac, you may need to download XQuartz first at https://www.xquartz.org/
+install.packages("hrbrthemes")
+library(hrbrthemes)
